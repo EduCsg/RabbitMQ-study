4970aabc8fe4ebb323bc56a86032efe312ef8d6a
EduCsg <eduardocasagrande71@gmail.com>
2024-06-27 01:10:21 -0300

    First commit

---
+++ src/main/java/com/example/rabbit_simple_project/RabbitSimpleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbit_simple_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitSimpleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitSimpleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rabbit_simple_project/RabbitSimpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbit_simple_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitSimpleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
